ec99060a1f5b5b0e6ce777bf3178423c42002ab9
Manas <isheremv@gmail.com>
2025-02-09 22:46:11 +0530

    view service for partner

--- middleware.js
+++ middleware.js
@@ -12,14 +12,17 @@ export function middleware(req) {
     "/api/partner/sign-up",
     "/api/admin/login",
     "/api/admin/sign-up",
-    "/api/service"
   ];
-  if (method==="POST" || !arr.includes(path)) {
+
+  if (method === "POST" && !arr.includes(path))
     return auth(req);
-  } else return NextResponse.next();
+  else if (path === "/api/partner/service") {
+    return auth(req);
+  }
+  else return NextResponse.next();
 }
 
 export const config = {
-    runtime: "nodejs",
-    matcher: ["/api/:path*"],
+  runtime: "nodejs",
+  matcher: ["/api/:path*"],
 };
