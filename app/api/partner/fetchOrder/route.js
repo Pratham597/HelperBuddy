7cac925a787f23c345c0b404abe7e9d73d488710
Pratham597 <prathammaheshwari20@gmail.com>
2025-02-12 01:08:18 +0530

    Fetching Order Implemented for Partner

--- app/api/partner/fetchOrder/route.js
+++ app/api/partner/fetchOrder/route.js
@@ -2,10 +2,42 @@ import PartnerService from "@/models/PartnerService";
 import ServiceOrder from "@/models/ServiceOrder";
 import { NextResponse } from "next/server";
 
-export const POST=async (req)=>{
-    const userId=req.headers.get("userId");
-    if(!userId) return NextResponse.json({error:"User unauthorized"},{status:404});
-    const patnerservice=await PartnerService.find({partner:userId});
-    const serviceOrder=await ServiceOrder.find({})
+export const POST = async (req) => {
+  const userId = req.headers.get("userId");
+  if (!userId)
+    return NextResponse.json({ error: "User unauthorized" }, { status: 401 });
 
-}
+  const partnerServices = await PartnerService.find({ partner: userId }).select(
+    "service pincode"
+  );
+
+  if (partnerServices.length === 0) {
+    return NextResponse.json(
+      { error: "No services found for the partner" },
+      { status: 404 }
+    );
+  }
+
+  //  2: Extract the  details (service and pincode)
+  const partnerServiceIds = partnerServices.map((item) => item.service);
+  const partnerPincodeMap = partnerServices.reduce((acc, item) => {
+    if (!acc[item.service]) {
+      acc[item.service] = new Set();
+    }
+    item.pincode.forEach((pincode) => acc[item.service].add(pincode));
+    return acc;
+  }, {});
+
+  const serviceOrders = await ServiceOrder.find({
+    partner: null, 
+    service: { $in: partnerServiceIds }, 
+    pincode: {
+      $in: partnerServices
+        .filter((service) => partnerPincodeMap[service.service])
+        .map((service) => partnerPincodeMap[service.service])
+        .flat(),
+    },
+  });
+
+  return NextResponse.json({ serviceOrders });
+};
--- app/api/service/[id]/available/route.js
+++ app/api/service/[id]/available/route.js
@@ -8,7 +8,7 @@ export const GET= async (req,{params}) => {
     await connectDB();
     const pincode = req.nextUrl.searchParams.get("pincode");
     if(!pincode) return NextResponse.json({error:"Pincode required"}, {status:400});
-    const partnerService=await PartnerService.findOne({service:id,pincode:{$in:pincode}});
-    if(!partnerService) return NextResponse.json({error:"Service not available in this area"}, {status:404});
+    const count=await PartnerService.countDocuments({service:id,pincode:{$in:[pincode]}});
+    if(count==0) return NextResponse.json({error:"Service not available in this area"}, {status:404});
     return NextResponse.json({success:"Service available in this area"}, {status:200});
 }
