95333143d2aa57e00577ea3a202a4b8682497f5f
Pratham597 <prathammaheshwari20@gmail.com>
2025-02-13 00:19:27 +0530

    Changes done in backend

--- app/api/user/checkout/route.js
+++ app/api/user/checkout/route.js
@@ -19,14 +19,13 @@ export const POST = async (req, res) => {
   }
   const data = await req.json();
   for (let i = 0; i < data.length; i++) {
-    const service = data[i];
+    const service = data.service[i];
     const serviceId = service.serviceId;
-    const pincode = service.pincode;
-    if(!serviceId || !pincode || !service.timeline || !service.address){
+    if(!serviceId || !service.timeline){
       return NextResponse.json({ error: "Invalid data" }, { status: 400 });
     }
   }
-  if(!data.totalAmount) return NextResponse.json({error:"Total amount required"},{status:403});
+  if(!data.totalAmount ||  !data.pincode || !data.address) return NextResponse.json({error:"Total amount required"},{status:403});
   const orderId = await generateOrderId(data.totalAmount);
   const booking=new Booking({
     user: userId,
@@ -37,6 +36,10 @@ export const POST = async (req, res) => {
   for (let i = 0; i < data.length; i++) {
     const service = data[i];
     service.booking=booking._id;
+    service.pincode=data.pincode
+    service.address=data.address
+    service.timeline=data[i].timeline
+    service.service=service.serviceId;
     const serviceOrder=new ServiceOrder(service);
     await serviceOrder.save();
   }
@@ -49,7 +52,7 @@ export const GET=async (req)=>{
   if(!user){
     return NextResponse.json({error:"User unauthorized"},{status:403});
   }
-  const bookings=await Booking.find({user:userId});
-  const service=await ServiceOrder.find({})
-  return NextResponse.json({bookings});
+  const bookings=await Booking.find({user:userId}).select("_id");
+  const service=await ServiceOrder.find({booking:{$in:bookings}}).populate("partner").populate("service")
+  return NextResponse.json({service});
 }
