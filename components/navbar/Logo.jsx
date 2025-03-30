d9b5631f0b2c5314937afc1a733e8bf4d15830c6
smitmarakna <smitmarakna1709@gmail.com>
2025-03-30 23:00:52 +0530

    logo size increased

--- components/navbar/Logo.jsx
+++ components/navbar/Logo.jsx
@@ -8,7 +8,7 @@ const Logo = () => {
 	return (
 		<Link href="/" className="flex items-center space-x-6">
 			<Image
-				src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=656,h=651,fit=crop/AzGeNv8QxRTqXJan/img_20241225_095321_917-AMqlqLEqp3tk7qEe.webp" // Ensure logo.png is in the "public" folder
+				src="/logo.png" // Ensure logo.png is in the "public" folder
 				alt="HelperBuddy Logo"
 				width={56} // Adjust based on your design
 				height={56}
